294b360ef2b09cf17d60ccf51c6663109dadba27
Lakshya <lakshyacollegepersonal@gmail.com>
2025-10-13 17:25:35 +0530

    learn string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -21,3 +21,14 @@ console.log(newString);
 
 const anotherString=gameName.slice(-8,4);
 console.log(anotherString);
+
+
+const newStringOne="   hitesh  ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url ="https://hitesh.com/hitesh%20choudhary";
+console.log(url.replace('%20','-'));
+console.log(url.includes('hitesh'));
+console.log(gameName.split(''));
